200d7418de4caad47426d20ad76028333cd717c6
doganaybalaban <dbalaban1907@gmail.com>
2024-08-29 21:26:06 +0300

    auth jwt and login register sections

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+connectDB();
+
+// Init Middleware
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+//Defined Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
