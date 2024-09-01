7dc42c8bf0a4f0155b8a41270e5b432515a7a684
doganaybalaban <dbalaban1907@gmail.com>
2024-09-02 01:22:00 +0300

    start client frontend side

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+        <section className="container">
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </>
+    </Router>
+  );
+};
+
+export default App;
---
+++ client/src/components/auth/Register.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const { name, email, password, confirmPassword } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+    } else {
+      //   const newUser = {
+      //     name,
+      //     email,
+      //     password,
+      //   };
+      //   try {
+      //     const config = {
+      //       headers: {
+      //         "Content-Type": "application/json",
+      //       },
+      //     };
+      //     const body = JSON.stringify(newUser);
+      //     const res = await axios.post("/api/users", body, config);
+      //     console.log(res.data);
+      //   } catch (error) {
+      //     console.error(error.response.data);
+      //   }
+      console.log("success");
+    }
+  };
+  return (
+    <>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            name="name"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+            name="email"
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+            placeholder="Password"
+            name="password"
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => onChange(e)}
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </>
+  );
+};
+
+export default Register;
